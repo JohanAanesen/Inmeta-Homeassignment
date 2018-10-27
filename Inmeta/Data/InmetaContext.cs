a4cbb730d977d0b0f687dfc74eb56def5ab789ce
Johan Aanesen <johanaan@stud.ntnu.no>
2018-10-27 08:39:06 +0200

    initial commit, rest thing now running for customer at least. Took a while to figure out the DB connection

---
+++ Inmeta/Data/InmetaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Inmeta.Data;
+
+namespace Inmeta.Models
+{
+    public class InmetaContext : DbContext
+    {
+        public InmetaContext (DbContextOptions<InmetaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Inmeta.Data.Customer> Customer { get; set; }
+    }
+}
